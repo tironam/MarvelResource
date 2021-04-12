987fd4d0a0a2c0eeb181428d9ca44e6011d6fca5
Michael <tironamichael@gmail.com>
2021-04-12 16:51:05 -0700

    Moved app.js back to main file

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import Navbar from './components/Navbar';
+import ProjectCard from './components/ProjectCard';
+
+const App = () => {
+
+  return (
+    <div>
+      <Navbar />
+      <ProjectCard />
+    </div>
+    // <div>Hello World</div>
+  );
+};
+
+export default App;
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const ProjectCard = () => {
-    
+
     return (
-        <div className="card" style="width: 18rem;">
-            <img src="https://lumiere-a.akamaihd.net/v1/images/p_thefalconandthewintersoldier_disneyplus_20653-1_a0638b8c.jpeg" className="card-img-top" alt="The Falcon and the Winter Soldier poster">
+        <div className="card" style={{ width: '22rem' }}>
+            <img src="https://lumiere-a.akamaihd.net/v1/images/p_thefalconandthewintersoldier_disneyplus_20653-1_a0638b8c.jpeg" className="card-img-top" alt="The Falcon and the Winter Soldier poster" />
                 <div className="card-body">
                     <h5 className="card-title">The Falcon and the Winter Soldier</h5>
                     <p className="card-text">Following the events of “Avengers: Endgame,” Sam Wilson/Falcon (Anthony Mackie) and Bucky Barnes/Winter Soldier (Sebastian Stan) team up in a global adventure that tests their abilities—and their patience—in Marvel Studios’ “The Falcon and The Winter Soldier.” </p>
