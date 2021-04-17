b6f70360a5f61701eb93982e34d86f34a38418f7
Michael <tironamichael@gmail.com>
2021-04-17 16:26:16 -0700

    Started on MCUPhases page

---
+++ src/pages/McuPhases/MCUPhases.js
@@ -0,0 +1,17 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import Navbar from '../components/Navbar';
+import ProjectCard from '../components/ProjectCard';
+
+const MCUPhases = props => {
+    constructor(props) {
+        super(props)
+    }
+
+    return (
+        <div>
+            <Navbar />
+            <ProjectCard />
+        </div>
+    )
+}
