0f948b129cd5c7d2d7cd0e6ca156c1ec3819eb3e
Michael <tironamichael@gmail.com>
2021-04-19 20:43:59 -0700

    Created MovieCard

---
+++ src/components/MovieCard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const MovieCard = props => {
+
+    return (
+        <div className="card" style={{ width: '22rem' }}>
+            <img src={props.poster} className="card-img-top" alt={props.alt} />
+                <div className="card-body">
+                    <h5 className="card-title">{props.title}</h5>
+                    <p className="card-text">{props.synopsis}</p>
+                </div>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">Release Date: {props.date}</li>
+                    <li className="list-group-item">Director: {props.director}</li>
+                    <li className="list-group-item">Writer: {props.writer}</li>
+                </ul>
+                {/* <div className="card-body">
+                    <a href="#" className="card-link">Card link</a>
+                    <a href="#" className="card-link">Another link</a>
+                </div> */}
+        </div>
+    );
+};
+
+export default MovieCard; 
