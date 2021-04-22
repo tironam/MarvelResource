7632f396a7603ad9e9a8fd441d1a74ed28fdde47
Michael <tironamichael@gmail.com>
2021-04-22 13:14:28 -0700

    Adjusted Navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -22,42 +22,10 @@ const Navbar = () => {
     const toggle = () => setIsOpen(!isOpen);
 
     return (
-        <NavB className="navbar navbar-expand-md navbar-light bg-light">
-            {/* <div className="container-fluid">
-                    <NavLink to='/Home' className="navbar-brand" >
-                        MCU & You
-                    </NavLink>
-                <NavbarToggler onClick={toggle} className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </NavbarToggler>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav"> */}
-                        {/* <a className="nav-link active" aria-current="page" href="#">Home</a> */}
-                        {/* <NavLink to='/MCUPhases' className="nav-link">
-                            The MCU So Far
-                        </NavLink>
-                        <NavLink to='/Upcoming' className="nav-link">
-                            What's Next?
-                        </NavLink>
-                        <NavLink to='DisneyParks' className="nav-link">
-                            Marvel in the Disney Parks
-                        </NavLink>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown link
-          </a>
-                            <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                    </div>
-                </div>
-            </div> */}
+        // <NavB className="navbar navbar-expand-md navbar-light bg-light">
             <NavB color="light" light expand="md">
                 <NavbarBrand href="/">MCU & You</NavbarBrand>
-                <NavbarToggler onClick={toggle} />
+                <NavbarToggler onClick={toggle} className="mr-2" />
                 <Collapse className="navbar-right" isOpen={isOpen} navbar>
                     <Nav className="mr-auto" navbar>
                         <UncontrolledDropdown nav inNavbar>
@@ -78,13 +46,15 @@ const Navbar = () => {
                             </DropdownMenu>
                         </UncontrolledDropdown>
                         <NavItem>
-                            <NavL href="Upcoming">What's Next?</NavL>
+                            <NavL href="/Upcoming">What's Next?</NavL>
+                        </NavItem>
+                        <NavItem>
+                            <NavL href="/DisneyParks">Marvel & The Disney Parks</NavL>
                         </NavItem>
                     </Nav>
-                    <NavbarText>Marvel & The Disney Parks</NavbarText>
                 </Collapse>
             </NavB>
-        </NavB>
+        // </NavB>
     )
 };
 
