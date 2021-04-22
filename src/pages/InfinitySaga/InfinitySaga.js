5a2f38569a233dc31cbb0ae7db8cc03f46ad246a
Michael <tironamichael@gmail.com>
2021-04-22 15:00:56 -0700

    Adjust button colors

--- src/pages/InfinitySaga/InfinitySaga.js
+++ src/pages/InfinitySaga/InfinitySaga.js
@@ -4,7 +4,7 @@ import Navbar from '../../components/Navbar';
 import ShowCard from '../../components/ShowCard';
 import MovieCard from '../../components/MovieCard';
 import Phase1 from '../../components/Phase1';
-import { UncontrolledCollapse, Button, CardBody, Card } from 'reactstrap';
+import { Button } from 'reactstrap';
 import './InfinitySaga.css';
 
 const InfinitySaga = () => {
@@ -15,18 +15,22 @@ const InfinitySaga = () => {
     return (
         <div>
             <h1 className="text">The Infinity Saga</h1>
-            <div>
-                <Button className="phaseBtn" onClick={toggle} color="primary" id="phase1" style={{ marginBottom: '1rem' }}>
+            <div className="text">
+                <Button className="phaseBtn phase1" color="primary" href="#phase1" style={{ marginBottom: '1rem' }}>
                     Phase 1
                 </Button>
-                <Button className="phaseBtn" color="primary" id="phase2" style={{ marginBottom: '1rem' }}>
+                <Button className="phaseBtn phase2" color="info" href="#phase2" style={{ marginBottom: '1rem' }}>
                     Phase 2
                 </Button>
-                <Button className="phaseBtn" color="primary" id="phase3" style={{ marginBottom: '1rem' }}>
+                <Button className="phaseBtn phase3" color="danger" href="#phase3" style={{ marginBottom: '1rem' }}>
                     Phase 3
                 </Button>
             </div>
+            <section id="phase1"></section>
             <Phase1 />
+            <section id="phase2"></section>
+            <Phase1 />
+            <section id="phase3"></section>
         </div>
     );
 }
