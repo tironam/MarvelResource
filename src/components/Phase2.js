9edee66adcf04b22383854b15b682f5cefbc2069
Michael <tironamichael@gmail.com>
2021-04-22 15:30:19 -0700

    Fleshed out Phase2 fully

--- src/components/Phase2.js
+++ src/components/Phase2.js
@@ -7,13 +7,58 @@ const Phase2 = () => {
             <h1 className="text">Phase 2</h1>
             <div className="row text-center">
                 <MovieCard
-                    poster=""
-                    title=""
-                    synopsis=""
-                    date=""
-                    director=""
-                    writer=""
-                    trailer=""
+                    poster="https://terrigen-cdn-dev.marvel.com/content/prod/1x/ironman3_lob_crd_01_11.jpg"
+                    title="Iron Man 3"
+                    synopsis="When Stark finds his personal world destroyed at his enemy's hands, he embarks on a harrowing quest to find those responsible. This journey, at every turn, will test his mettle. With his back against the wall, Stark is left to survive by his own devices, relying on his ingenuity and instincts to protect those closest to him. As he fights his way back, Stark discovers the answer to the question that has secretly haunted him: does the man make the suit or does the suit make the man"
+                    date="May 3, 2013"
+                    director="Shane Black"
+                    writer="Drew Pearce, Shane Black"
+                    trailer="https://www.youtube.com/watch?v=YLorLVa95Xo"
+                />
+                <MovieCard
+                    poster="https://terrigen-cdn-dev.marvel.com/content/prod/1x/thorthedarkworld_lob_crd_02.jpg"
+                    title="Thor: The Dark World"
+                    synopsis="Thor fights to restore order across the cosmos...but an ancient race led by the vengeful Malekith returns to plunge the universe back into darkness. Faced with an enemy that even Odin and Asgard cannot withstand, Thor must embark on his most perilous and personal journey yet, one that will reunite him with Jane Foster and force him to sacrifice everything to save us all."
+                    date="November 8, 2013"
+                    director="Alan Taylor"
+                    writer="Christopher L. Yost, Christopher Markus, Stephen McFeely"
+                    trailer="https://www.youtube.com/watch?v=npvJ9FTgZbM"
+                />
+                <MovieCard
+                    poster="https://terrigen-cdn-dev.marvel.com/content/prod/1x/captainamericathewintersoldier_lob_crd_01_2.jpg"
+                    title="Captain America: The Winter Soldier"
+                    synopsis="Steve Rogers, aka Captain America, is living quietly in Washington, D.C. and trying to adjust to the modern world. But when a S.H.I.E.L.D. colleague comes under attack, Steve becomes embroiled in a web of intrigue that threatens to put the world at risk. Joining forces with the Black Widow, Captain America struggles to expose the ever-widening conspiracy while fighting off professional assassins sent to silence him at every turn. When the full scope of the villainous plot is revealed, Captain America and the Black Widow enlist the help of a new ally, the Falcon. However, they soon find themselves up against an unexpected and formidable enemy--the Winter Soldier."
+                    date="April 4, 2014"
+                    director="Anthony Russo, Joe Russo"
+                    writer="Christopher Markus, Stephen McFeely"
+                    trailer="https://www.youtube.com/watch?v=7SlILk2WMTI"
+                />
+                <MovieCard
+                    poster="https://terrigen-cdn-dev.marvel.com/content/prod/1x/guardiansofthegalaxy_lob_crd_03_0.jpg"
+                    title="Guardians of the Galaxy"
+                    synopsis="Brash adventurer Peter Quill finds himself the object of an unrelenting bounty hunt after stealing a mysterious orb coveted by Ronan, a powerful villain with ambitions that threaten the entire universe. To evade the ever-persistent Ronan, Quill is forced into an uneasy truce with a quartet of disparate misfits--Rocket, a gun-toting raccoon; Groot, a tree-like humanoid; the deadly and enigmatic Gamora; and the revenge-driven Drax the Destroyer. But when Quill discovers the true power of the orb and the menace it poses to the cosmos, he must do his best to rally his ragtag rivals for a last, desperate stand--with the galaxy's fate in the balance."
+                    date="August 1, 2014"
+                    director="James Gunn"
+                    writer="James Gunn, Nicole Perlman"
+                    trailer="https://www.youtube.com/watch?v=d96cjJhvlMA"
+                />
+                <MovieCard
+                    poster="https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersageofultron_lob_crd_03_0.jpg"
+                    title="Avengers: Age of Ultron"
+                    synopsis="When Tony Stark tries to jumpstart a dormant peacekeeping program, things go awry and Earth’s Mightiest Heroes, including Iron Man, Captain America, Thor, The Incredible Hulk, Black Widow and Hawkeye, are put to the ultimate test as the fate of the planet hangs in the balance. As the villainous Ultron emerges, it is up to the Avengers to stop him from enacting his terrible plans, and soon uneasy alliances and unexpected action pave the way for an epic and unique global adventure."
+                    date="May 1, 2015"
+                    director="Joss Whedon"
+                    writer="Joss Whedon"
+                    trailer="https://www.youtube.com/watch?v=tmeOjFno6Do"
+                />
+                <MovieCard
+                    poster="https://terrigen-cdn-dev.marvel.com/content/prod/1x/ant-man_lob_crd_01_9.jpg"
+                    title="Ant-Man"
+                    synopsis="Armed with the astonishing ability to shrink in scale but increase in strength, master thief Scott Lang must embrace his inner-hero and help his mentor, Doctor Hank Pym, protect the secret behind his spectacular Ant-Man suit from a new generation of towering threats. Against seemingly insurmountable obstacles, Pym and Lang must plan and pull off a heist that will save the world."
+                    date="July 17, 2015"
+                    director="Peyton Reed"
+                    writer="Edgar Wright, Joe Cornish, Adam McKay, Paul Rudd"
+                    trailer="https://www.youtube.com/watch?v=pWdKf3MneyI"
                 />
             </div>
         </div>
