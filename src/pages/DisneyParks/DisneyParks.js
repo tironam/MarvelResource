15b9d4574aa2c8c422f605ed9b326d35628ed2c9
Michael <tironamichael@gmail.com>
2021-04-28 18:24:32 -0700

    Created theme park components

--- src/pages/DisneyParks/DisneyParks.js
+++ src/pages/DisneyParks/DisneyParks.js
@@ -1,12 +1,17 @@
 import { render } from '@testing-library/react';
 import React from 'react';
 import Navbar from '../../components/Navbar';
+import AttractionCard from '../../components/AttractionCard';
 
 const DisneyParks = () => {
 
     return (
         <div>
-            <h1>Disney Parks Marvel</h1>
+            <h1 className="text">Disney Parks Marvel</h1>
+            <h5 className="text">Disney California Adventure</h5>
+            <AttractionCard
+                
+            />
         </div>
     )
 }
