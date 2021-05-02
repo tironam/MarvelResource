6d41535c0b6fd5c4c87b67c8610f48170f5952f2
Michael <tironamichael@gmail.com>
2021-05-01 17:15:46 -0700

    Updated attraction card properties

--- src/components/AttractionCard.js
+++ src/components/AttractionCard.js
@@ -3,16 +3,15 @@ import React from 'react';
 const AttractionCard = props => {
 
     return (
-        <div className="card" style={{ width: '22rem' }}>
+        <div className="card" style={{ width: '22rem', backgroundColor: 'transparent' }} >
             <img src={props.image} className="card-img-top" alt={props.alt} />
             <div className="card-body">
-                <h5 className="card-title">{props.name}</h5>
-                <p className="card-text">{props.locale}</p>
-                <p className="card-text">{props.synopsis}</p>
+                <h5 className="card-title info">{props.name}</h5>
+                <p className="card-text info">{props.synopsis}</p>
             </div>
-            <ul className="list-group list-group-flush">
-                <li className="list-group-item">Release Date: {props.date}</li>
-                <li className="list-group-item">Park: {props.director}</li>
+            <ul className="list-group list-group-flush" >
+                <li className="list-group-item info" style={{ backgroundColor: 'transparent' }}>{props.locale}</li>
+                <li className="list-group-item info" style={{ backgroundColor: 'transparent' }}>Opening Date: {props.date}</li>
             </ul>
         </div>
     );
--- src/pages/DisneyParks/DisneyParks.js
+++ src/pages/DisneyParks/DisneyParks.js
@@ -8,10 +8,16 @@ const DisneyParks = () => {
     return (
         <div>
             <h1 className="text">Disney Parks Marvel</h1>
-            <h5 className="text">Disney California Adventure</h5>
-            <AttractionCard
-                
-            />
+            <h5 className="text">The Disneyland Resort</h5>
+            <div className="row text-center">
+                <AttractionCard
+                    image="https://www.slashfilm.com/wp/wp-content/images/Guardians-of-the-Galaxy-Mission-Breakout-easter-eggs.jpg"
+                    name="Guardians of the Galaxy - Mission: BREAKOUT!"
+                    synopsis="Tanaleer Tivan aka The Collector (Benecio Del Toro) has brought his collection to Tera for the first time, and you're invited to see his newest additions... The Guardians of the Galaxy. Held captive high up in his fortress, Rocket (Bradley Cooper) escapes to enlist your help to break out Star-Lord (Chris Pratt), Gamora (Zoe Saldana), Drax (Dave Bautista), and Groot to the soundtrack of six pop hits, each offering unique scenes and drop sequences."
+                    locale="Located in Disney California Adventure's Hollywood Land. Soon to be part of Avengers Campus."
+                    date="May 27, 2017"
+                />
+            </div>
         </div>
     )
 }
