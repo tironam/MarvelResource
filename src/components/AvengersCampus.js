b6def54b1e40f0c1a56c9860c113c5cc6cfd797a
Michael <tironamichael@gmail.com>
2021-05-03 17:52:51 -0700

    Finsihed adding content to AvenversCampus component

--- src/components/AvengersCampus.js
+++ src/components/AvengersCampus.js
@@ -9,33 +9,67 @@ const AvengersCampus = () => {
                 <AttractionCard
                     image="https://cdn1.parksmedia.wdprapps.disney.com/media/blog/wp-content/uploads/2019/08/Image_DLR_AvengersCampusBlog.jpg"
                     name="Avengers Campus"
-                    synopsis="Avengers Campus is part of the Global Avengers Initiative to find new recruits to help fight the battles ahead. Here, guests will get to meet some of their favorite heroes in attractions, encounters throughout the land, shows, or even swinging above their heads. This marks the first Marvel-themed land in a Disney park and one of three planned between Disney California Adventure, Hong Kong Disneyland, and Walt Disney Studios Park at Disneyland Paris."
+                    synopsis="Avengers Campus is part of the Global Avengers Initiative to find new recruits to help fight the battles ahead. Here, guests will get to meet some of their favorite heroes in attractions, encounters throughout the land, shows, or even swinging above their heads."
+                    info="This marks the first Marvel-themed land in a Disney park and one of three planned between Disney California Adventure, Hong Kong Disneyland, and Walt Disney Studios Park at Disneyland Paris. The new land was originally set to open in the summer of 202, but was delayed due to the pandemic. While it's now set to open in 2021, Phase 2 of the land is expected to happen at some point down the line which will see a new Avengers-themed attraction that will be attached to the Avengers Headquarters building that will be part of the Phase 1 opening."
                     locale="Disney California Adventure"
                     date="June 4, 2021"
                 />
             </div>
-            <h4 className="text">The Attractions of Avengers Campus:</h4>
+            <h4 className="text">The Attractions of Avengers Campus</h4>
             <div className="row text-center">
                 <AttractionCard
                     image="https://www.slashfilm.com/wp/wp-content/images/Guardians-of-the-Galaxy-Mission-Breakout-easter-eggs.jpg"
                     name="Guardians of the Galaxy - Mission: BREAKOUT!"
-                    synopsis="Tanaleer Tivan aka The Collector (Benecio Del Toro) has brought his collection to Tera for the first time, and you're invited to see his newest additions... The Guardians of the Galaxy. Held captive high up in his fortress, Rocket (Bradley Cooper) escapes to enlist your help to break out Star-Lord (Chris Pratt), Gamora (Zoe Saldana), Drax (Dave Bautista), and Groot to the soundtrack of six pop hits, each offering unique scenes and drop sequences."
+                    synopsis="Tanaleer Tivan aka The Collector has brought his collection to Tera for the first time, and you're invited to see his newest additions... The Guardians of the Galaxy. Held captive high up in his fortress, Rocket escapes to enlist your help to break out his fellow Guardians to the soundtrack of six pop hits, each offering unique scenes and drop sequences."
+                    info='Benecio Del Toro, Bradley Cooper, Chris Pratt, Zoe Saldana, and Dave Bautista all reprise their roles for the attraction. The scenes for the attraction were largely filmed during production of "Guardians of the Galaxy Vol. 2. The Fortress is filled with Marvel and Disney Park easter eggs, including a destroyed Ultron sentry, Inhuman Terrigen Crystals, and Figment from the "Journey Into Imagination" attraction at EPCOT.'
                     locale="Located in Disney California Adventure's Hollywood Land. Soon to be part of Avengers"
                     date="May 27, 2017"
+                />
+                <AttractionCard
+                    image="https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/1600/900/75/dam/wdpro-assets/dlr/parks-and-tickets/attractions/disney-california-adventure/guardians-galaxy-mission-breakout-after-dark/new-monsters-after-dark-16x9.jpg?1616105894503"
+                    name="Guardians of the Galaxy - Monsters After Dark"
+                    synopsis="After escaping the Collector's Fortress, Rocket returns to rescue a left behind Groot while the once captive creatures now run loose."
+                    info='Set to the backdrop of an original song by Tyler Bates, the composer of the films. This version of the attraction can be found after dark during the Halloween season and acts as a sequel to the story of the regular version of the attraction. The main monster in the attraction is the Fire Dragon that belonged to Surtur from "Thor: Ragnarok"'
+                    locale="Located in Disney California Adventure's Hollywood Land. Soon to be part of Avengers"
+                    date="September 15, 2017"
                 />
                 <AttractionCard
                     image="https://cdn1.parksmedia.wdprapps.disney.com/media/blog/wp-content/uploads/2019/08/sdhbvwuy4y9u133-1024x469.jpg"
                     name="WEB SLINGERS: A Spider-Man Adventure"
                     synopsis="The Worldwide Engineering Brigade (or W.E.B. for short) is home to some of the brightest young minds in the world like Peter Parker, come together to help develop new technology for the greatest Super Heroes in the world. When Spider-Bots start running amok around Avengers Campus, it's up to you to board the WEB Slinger to help Spider-Man shoot webs and wrangle them up."
-                    locale="Avengers Campus"
+                    info="Tom Holland reprises his role as Peter Parker/Spider-Man for the attraction. Above the show building, you'll be able to see Spider-Man swing off a web and flip through the air thanks to a groundbreaking stuntronic designed by Walt Disney Imagineering. Spider-Man sports a suit that's inspired by his MCU look, but unique to the Disney Parks and Resorts that was designed by Ryan Meinerding, the head of Visual Development for Marvel Studios. Meinerding designed Spidey's signature look for the MCU."
                     date="June 4, 2021"
                 />
                 <AttractionCard 
                     image="https://cdn1.parksmedia.wdprapps.disney.com/media/blog/wp-content/uploads/2019/08/sdhbvwuy4y9u137.jpg"
                     name="Untitled Avengers Attraction"
-                    synopsis="Enter Avengers Headquarters, where you'll board a Quinjet and join Earth's Mightiest Heroes on an exciting journey to save the day. This E-Ticket attraction promises to feature an innovative ride system."
-                    locale="Avengers Campus"
+                    synopsis="Enter Avengers Headquarters, where you'll board a Quinjet and join Earth's Mightiest Heroes on an exciting journey to save the day."
+                    info="This E-Ticket attraction promises to feature an innovative ride system. Concept art for the attraction showed the guest's seats in the Quinjet splitting off and moving around a battle independently, which may hint at the ride system in mind. It also depicted a battle happening in Wakanda, but it's unclear if that will still be a location destined for the attraction following Chadwick Boseman's untimely passing."
                     date="Phase two of Avengers Campus (TBA)"
+                />
+            </div>
+            <h4 className="text">Heroic Encounters in Avengers Campus</h4>
+            <div className="row text-center">
+                <AttractionCard
+                    image="https://cdn1.parksmedia.wdprapps.disney.com/media/blog/wp-content/uploads/2021/04/avcd20394802857928470293402394.jpg"
+                    name="Ancient Sanctum"
+                    synopsis="This location in the hills of Los Angeles was said to be flowing with mysterious energies before Howard Stark built a Stark Industries complex on the site. Decades later, Tony Stark invited Doctor Strange to the campus, who has uncovered the source for the mysterious energy and activity - a once hidden ancient California sanctum. Here, you'll witness the master of the mystic arts practice his magic and meet him face to face."
+                    info=""
+                    date="June 4, 2020"
+                />
+                <AttractionCard 
+                    image="https://cdn1.parksmedia.wdprapps.disney.com/media/blog/wp-content/uploads/2021/04/avcd023948209342039482039482034928342fi-624x352.jpg"
+                    name="Avengers Headquarters"
+                    synopsis="As the Avengers Quinjet sits atop the headquarters building, so do heroes like Iron Man, Captain Marvel, Black Widow, and Captain America. They keep watch for any potential threats, like Taskmaster and his mercenaries, who may be plotting to break-in. They'll come toe-to-toe along the rooftops in an exciting show!"
+                    info="This building will become the entrance to the future Avengers E-Ticket attraction when Phase 2 of the land opens. Iron Man will be sporting a Mark 80 suit, which is unique to the Disney Parks. Other heroes, including Black Panther and his Dora Milaje, will be among the other heroic encounters that can be found around the Headquarters and the land."
+                />
+                <AttractionCard 
+                    image="https://cdn1.parksmedia.wdprapps.disney.com/media/blog/wp-content/uploads/2021/04/avcd29387209850298354028273409234.jpg"
+                    name="Pym Test Kitchen"
+                    synopsis="This old lab facility has been transformed into a revolutionary eatery by Ant-Man and The Wasp and their team of research chefs who've turned their size-altering experiments onto a new subject - food! Discover exciting menu items that have grown and shrunk to extraordinary sizes with unique ambience that could only be brought to life with the amazing Pym Particle."
+                    info="Ant-Man and The Wasp will be making their Disney Parks character debut with Avengers Campus. Many of the overgrown menu items will even include replicas of their Pym Particle-packed discs that are responsible for changing the size of items. The quick service restaurant will feature soda fountains powered by extra large soda cans, giant condiment bottles, and outdoor lighting from giant Christmas lights."
+                    locale=""
+                    date="June 4, 2021"
                 />
             </div>
         </div>
