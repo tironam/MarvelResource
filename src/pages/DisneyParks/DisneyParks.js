720162a52b721ceca22cce4005b9ed7e09bd38ac
Michael <tironamichael@gmail.com>
2021-06-01 18:46:40 -0700

    Added navigation buttons to each page

--- src/pages/DisneyParks/DisneyParks.js
+++ src/pages/DisneyParks/DisneyParks.js
@@ -1,4 +1,5 @@
 import { render } from '@testing-library/react';
+import { Button } from 'reactstrap';
 import React from 'react';
 import Navbar from '../../components/Navbar';
 import AvengersCampus from '../../components/ThemeParks/AvengersCampus';
@@ -16,24 +17,41 @@ const DisneyParks = () => {
         <div>
             <ScrollUpButton />
             <h1 className="text">MARVEL IN THE DISNEY PARKS AND RESORTS</h1>
+            <div className="text">
+                <Button size="lg" className="phaseBtn phase1" color="danger" href="#DLR" style={{ marginBottom: '1rem' }}>
+                    Disneyland Resort
+                </Button>
+                <Button size="lg" className="phaseBtn phase2" color="danger" href="#WDW" style={{ marginBottom: '1rem' }}>
+                    Walt Disney World
+                </Button>
+                <Button size="lg" className="phaseBtn phase3" color="danger" href="#HKDL" style={{ marginBottom: '1rem' }}>
+                    Hong Kong Disneyland
+                </Button>
+                <Button size="lg" className="phaseBtn phase3" color="danger" href="#DLP" style={{ marginBottom: '1rem' }}>
+                    Disneyland Paris
+                </Button>
+                <Button size="lg" className="phaseBtn phase3" color="danger" href="#DCL" style={{ marginBottom: '1rem' }}>
+                    Disney Cruise Line
+                </Button>
+            </div>
             <h4 className="text"></h4>
-            <h2 className="text">The Disneyland Resort</h2>
+            <h2 className="text" id="DLR">The Disneyland Resort</h2>
             <div className="text-center">
                 <AvengersCampus />
             </div>
-            <div className="text-center">
+            <div className="text-center" id="DLR">
                 <DLR />
             </div>
-            <div className="text-center">
+            <div className="text-center" id="WDW">
                 <WDW />
             </div>
-            <div className="text-center">
+            <div className="text-center" id="HKDL">
                 <HK />
             </div>
-            <div className="text-center">
+            <div className="text-center" id="DLP">
                 <DLP />
             </div>
-            <div className="text-center">
+            <div className="text-center" id="DCL">
                 <DCL />
             </div>
         </div>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -14,19 +14,19 @@ const Home = () => {
                 <ImgButton 
                     href="/InfinitySaga"
                     image="https://cdn.mos.cms.futurecdn.net/TkGBPEiAxyXDphCpELDsQ7-1200-80.jpg"
-                    alt=""
+                    alt="Poster for Avengers: Endgame"
                     link="The Infinity Saga"
                 />
                 <ImgButton 
                     href="/NextSaga"
                     image="https://cdn3.whatculture.com/images/2020/09/286b83bfb0b66bea-600x338.jpg"
-                    alt=""
+                    alt="WandaVision Poster"
                     link="Phase 4 and Beyond"
                 />
                 <ImgButton
                     href="/DisneyParks"
                     image="https://cdn1.parksmedia.wdprapps.disney.com/media/blog/wp-content/uploads/2020/03/avc345789523428593406457567.jpg"
-                    alt=""
+                    alt="Avengers Campus concept art"
                     link="Marvel in the Disney Parks"
                 />
             </div>
