e1d5977d9b204fe5fb5b1dc4daa2b5f5044230dd
tironam <tironamichael@gmal.com>
2021-10-24 20:57:27 -0700

    Added UpdateModal to home page for site updates

--- src/components/UpdateModal.js
+++ src/components/UpdateModal.js
@@ -4,7 +4,8 @@ import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 const UpdateModal = (props) => {
     const {
         buttonLabel,
-        className
+        className,
+        title
     } = props;
 
     const [modal, setModal] = useState(false);
@@ -12,17 +13,16 @@ const UpdateModal = (props) => {
     const toggle = () => setModal(!modal);
 
     return (
-        <div>
-            <Button color="danger" onClick={toggle}>{buttonLabel}</Button>
+        <div style={{ "textAlign": "center", "color": "#cc0000" }}>
+            <Button color="danger" onClick={toggle} className="navbar-text" style={{ "fontSize": "25px"}}>{buttonLabel}</Button>
             <Modal isOpen={modal} toggle={toggle} className={className}>
-                <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+                <ModalHeader toggle={toggle}>{title}</ModalHeader>
                 <ModalBody>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    <ul>
+                        <li>Updated the poster for Hawkeye - 10.24.21</li>
+                        <li>Updated the release dates for Doctor Strange in the Multiverse of Madness, Thor: Love and Thunder, Black Panther: Wakanda Forever, The Marvels, and Ant-Man and the Wasp: Quantumania - 10.21</li>
+                    </ul>
                 </ModalBody>
-                <ModalFooter>
-                    <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
-                    <Button color="secondary" onClick={toggle}>Cancel</Button>
-                </ModalFooter>
             </Modal>
         </div>
     );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -10,7 +10,8 @@ const Home = () => {
             <h1 className="header-text" >The ultimate resource for new and seasoned fans of the Marvel Cinematic Universe</h1>
             <h2 className="black-text ">Discover the past and future of the MCU. Learn about the MCU-inspired lands and attractions at Disney Parks around the world. Get answers to your important questions. Everything you need to know is right here.</h2>
             <UpdateModal 
-                
+                buttonLabel="Updates"
+                title="What's new to the site?"
             />
             <BtnSection />
         </div>
