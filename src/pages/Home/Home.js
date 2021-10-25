5e4eaed2647592815fbdf020df4c4b1cc99e5172
tironam <tironamichael@gmal.com>
2021-10-24 20:38:29 -0700

    Created a new file for UpdateModal

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from '../../components/Navbar.js';
 import './Home.css';
 import BtnSection from '../../components/BtnSection.js';
+import '../../components/InfoModal.js';
 
 const Home = () => {
     return (
