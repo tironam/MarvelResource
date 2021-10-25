db09a79a7590d5534aa0dacf76270b347595e244
tironam <tironamichael@gmal.com>
2021-10-24 20:46:32 -0700

    Added a new UpdateModal component

--- src/components/UpdateModal.js
+++ src/components/UpdateModal.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-const ModalExample = (props) => {
+const UpdateModal = (props) => {
     const {
         buttonLabel,
         className
@@ -17,7 +17,7 @@ const ModalExample = (props) => {
             <Modal isOpen={modal} toggle={toggle} className={className}>
                 <ModalHeader toggle={toggle}>Modal title</ModalHeader>
                 <ModalBody>
-                    
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </ModalBody>
                 <ModalFooter>
                     <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
@@ -28,4 +28,4 @@ const ModalExample = (props) => {
     );
 }
 
-export default ModalExample;
+export default UpdateModal;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Navbar from '../../components/Navbar.js';
 import './Home.css';
 import BtnSection from '../../components/BtnSection.js';
-import '../../components/InfoModal.js';
+import UpdateModal from '../../components/UpdateModal'
 
 const Home = () => {
     return (
         <div>
             <h1 className="header-text" >The ultimate resource for new and seasoned fans of the Marvel Cinematic Universe</h1>
             <h2 className="black-text ">Discover the past and future of the MCU. Learn about the MCU-inspired lands and attractions at Disney Parks around the world. Get answers to your important questions. Everything you need to know is right here.</h2>
+            <UpdateModal 
+                
+            />
             <BtnSection />
         </div>
     )
